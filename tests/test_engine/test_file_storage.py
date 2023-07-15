ebf9ec2bbcbca58e6f8c4e39405aac5806d16eda
Mohamed Amine <ma.abahmane@gmail.com>
2023-07-15 11:58:48 +0100

    AIRbnb

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,47 @@
+""" file_storage test file """
+
+import unittest
+import models.engine
+from models.engine.file_storage import *
+
+
+class FileStorageTest(unittest.TestCase):
+    """ FileStorage testing class """
+
+    def test_file_storage(self):
+        """ testing the class BaseModel """
+
+        # test if instance belongs to its class
+        _object = FileStorage()
+
+        self.assertIsInstance(_object, FileStorage)
+
+        # test if object dictionary exists and is not none
+        _object = FileStorage()
+        objsD = _object.all()
+
+        self.assertEqual(type(objsD), dict)
+        self.assertIsNotNone(objsD)
+
+        # test if function save saves objects in dictionary correctly
+        dataBase = FileStorage()
+        obj_dict = dataBase.all()
+        _object = BaseModel()
+        obj_key = f"{BaseModel.__name__}.{_object.id}"
+
+        self.assertIsNotNone(obj_dict[obj_key])
+
+        # check if update time gets updated when saved
+        _object = BaseModel()
+        time1 = _object.updated_at
+        _object.save()
+        time2 = _object.updated_at
+
+        self.assertNotEqual(time1, time2)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
