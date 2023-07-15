d1b3aa06ad4793b5a130b9b75eab67d5ce18a164
Mohamed Amine <ma.abahmane@gmail.com>
2023-07-15 13:12:32 +0100

    AIRbnb

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,46 +1,46 @@
 #!/usr/bin/python3
-""" base_model test file """
+""" user test file """
 
 import unittest
 import models
-from models.base_model import *
+from models.user import *
 
 
-class BaseModelTest(unittest.TestCase):
-    """ BaseModel testing class """
+class UserTest(unittest.TestCase):
+    """ User testing class """
 
-    def test_BaseModel(self):
+    def test_User(self):
         """ testing the class BaseModel """
         
         # test if instance belongs to its class
-        _object = BaseModel()
+        _object = User()
 
-        self.assertIsInstance(_object, BaseModel)
+        self.assertIsInstance(_object, User)
 
         # test if 2 instance ids are unidentical
-        _object = BaseModel()
-        _object2 = BaseModel()
+        _object = User()
+        _object2 = User()
 
         self.assertNotEqual(_object, _object2)
         self.assertNotEqual(_object.id, _object2.id)
 
         # test if instance string is correctly formated
-        _object = BaseModel()
+        _object = User()
         _object_str = str(_object)
-        Sformat = f"[BaseModel] ({_object.id}) {_object.__dict__}"
+        Sformat = f"[User] ({_object.id}) {_object.__dict__}"
 
         self.assertEqual(Sformat, _object_str)
 
         # check if to_dict return a dictionary representation of instance
         # containing class name
-        _object = BaseModel()
+        _object = User()
         obj_dict = _object.to_dict()
         classN = obj_dict['__class__']
 
-        self.assertEqual(classN, 'BaseModel')
+        self.assertEqual(classN, 'User')
 
         # check if update time gets updated when saved
-        _object = BaseModel()
+        _object = User()
         time1 = _object.updated_at
         _object.save()
         time2 = _object.updated_at
